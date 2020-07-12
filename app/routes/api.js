e3817dc74ea652f1a6978d19bcb6c8c5d0ec4b81
Paul Bacchus <paul@paulbacchus.com>
2020-07-12 20:29:52 +0100

    Updated express server app to use express-promise-router.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const Router = require("express-promise-router");
+const db = require("database");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = new Router();
+
+/* GET all genes. */
+router.get("/", async (req, res) => {
+  const { rows } = await db.getAllGenes();
+  res.send(rows);
+});
+
+/* GET gene information by ID. */
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await db.getGeneInfoById(id);
+  res.send(rows[0]);
 });
 
 module.exports = router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const Router = require("express-promise-router");
+const db = require("database");
+
+const router = new Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
