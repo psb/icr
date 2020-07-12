bdf0f9fc2be9fc7bc6e38b8ee2dade83138924ee
Paul Bacchus <paul@paulbacchus.com>
2020-07-12 14:38:13 +0100

    Created table for gene families.

---
+++ data/sql/create_gene_families_table.sql
@@ -0,0 +1,22 @@
+-- Table: public.gene_families
+
+-- DROP TABLE public.gene_families;
+
+CREATE TABLE public.gene_families
+(
+    family character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    id integer NOT NULL DEFAULT nextval('gene_families_id_seq'::regclass),
+    gene_id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT unique_family_id PRIMARY KEY (id),
+    CONSTRAINT gene_id FOREIGN KEY (gene_id)
+        REFERENCES public.genes (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.gene_families
+    OWNER to postgres;
