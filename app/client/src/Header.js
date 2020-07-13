f8c9279fe41d723857becb8c353352170ae9bdf7
Paul Bacchus <paul@paulbacchus.com>
2020-07-13 18:38:01 +0100

    Filtering of genes by feature on the home page is working. However, the filtering action is unclear as to what is required, i.e. whether filtering is AND or OR.

--- app/client/src/Header.js
+++ app/client/src/Header.js
@@ -5,10 +5,14 @@ function Header() {
   return (
     <div className="row my-4 align-items-center">
       <div className="col-auto">
-        <img src={logo} alt="ICR logo" width="300" />
+        <a href="https://www.icr.ac.uk/">
+          <img src={logo} alt="ICR logo" width="300" />
+        </a>
       </div>
       <div className="col text-center">
-        <h1 className="display-4">Cancer Genes DB</h1>
+        <a href="/" className="text-body" style={{ "text-decoration": "none" }}>
+          <h1 className="display-4">Cancer Genes DB</h1>
+        </a>
       </div>
     </div>
   );
--- app/client/src/Home.js
+++ app/client/src/Home.js
@@ -3,8 +3,11 @@ import FeatureText from "./FeatureText";
 
 function Home() {
   const [genes, setGenes] = useState([]);
+  const [filteredGenes, setFilteredGenes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [showDrugTargets, setShowDrugTargets] = useState(true);
+  const [showEnzymes, setShowEnzymes] = useState(true);
 
   useEffect(() => {
     fetch("/api/genes")
@@ -17,6 +20,12 @@ function Home() {
       })
       .then((json) => {
         setGenes(json);
+        const newData = json.filter(
+          (gene) =>
+            gene.is_druggable === showDrugTargets ||
+            gene.is_enzyme === showEnzymes
+        );
+        setFilteredGenes(newData);
       })
       .catch((err) => {
         console.error(err);
@@ -27,43 +36,95 @@ function Home() {
       });
   }, []);
 
+  function toggleShowDrugTargets() {
+    const newData = genes.filter(
+      (gene) =>
+        gene.is_druggable === !showDrugTargets || gene.is_enzyme === showEnzymes
+    );
+    setFilteredGenes(newData);
+    setShowDrugTargets(!showDrugTargets);
+  }
+
+  function toggleShowEnzymes() {
+    const newData = genes.filter(
+      (gene) =>
+        gene.is_enzyme === !showEnzymes || gene.is_druggable === showDrugTargets
+    );
+    setFilteredGenes(newData);
+    setShowEnzymes(!showEnzymes);
+  }
+
   if (loading) {
     return "Loading data...";
   } else if (error) {
     return "Error loading data.";
   } else {
     return (
-      <div className="row">
-        {genes.map((gene) => (
-          <div className="col-3 my-2">
-            <div className="card">
-              <img
-                src={gene.image}
-                className="card-img-top"
-                alt={gene.short_name}
-              />
-              <div className="card-body">
-                <h4 className="card-title">{gene.short_name}</h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  Is a drug target?
-                  <FeatureText hasFeature={gene.is_druggable} />
-                </li>
-                <li className="list-group-item">
-                  Is an enzyme?
-                  <FeatureText hasFeature={gene.is_enzyme} />
-                </li>
-              </ul>
-              <div className="card-body">
-                <a href={"/" + gene.id} className="btn btn-primary btn-block">
-                  More information
-                </a>
+      <>
+        {/* Start feature filters */}
+        <div className="row mb-3">
+          <div className="col-auto ml-4">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="drugTargets"
+              aria-label="Checkbox to show drug targets"
+              checked={showDrugTargets}
+              onClick={toggleShowDrugTargets}
+            />
+            <label class="form-check-label" for="drugTargets">
+              Show drug targets
+            </label>
+          </div>
+          <div className="col-auto ml-4">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="enzymes"
+              aria-label="Checkbox to show enzymes"
+              checked={showEnzymes}
+              onClick={toggleShowEnzymes}
+            />
+            <label class="form-check-label" for="enzymes">
+              Show enzymes
+            </label>
+          </div>
+        </div>
+        {/* End feature filters */}
+        {/* Start gene cards */}
+        <div className="row">
+          {filteredGenes.map((gene) => (
+            <div className="col-3 my-2">
+              <div className="card">
+                <img
+                  src={gene.image}
+                  className="card-img-top"
+                  alt={gene.short_name}
+                />
+                <div className="card-body">
+                  <h4 className="card-title">{gene.short_name}</h4>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    Is a drug target?
+                    <FeatureText hasFeature={gene.is_druggable} />
+                  </li>
+                  <li className="list-group-item">
+                    Is an enzyme?
+                    <FeatureText hasFeature={gene.is_enzyme} />
+                  </li>
+                </ul>
+                <div className="card-body">
+                  <a href={"/" + gene.id} className="btn btn-primary btn-block">
+                    More information
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+        {/* End gene cards */}
+      </>
     );
   }
 }
