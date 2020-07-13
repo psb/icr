ab0afb7343b64296ee4136b60368e00bc44ab32b
Paul Bacchus <paul@paulbacchus.com>
2020-07-13 11:51:38 +0100

    Data fetching for home page working.

--- app/client/src/Home.js
+++ app/client/src/Home.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [genes, setGenes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/genes")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw response;
+        }
+      })
+      .then((json) => {
+        setGenes(json);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return "Loading data...";
+  } else if (error) {
+    return "Error loading data.";
+  } else {
+    return (
+      <ul>
+        {genes.map((gene) => (
+          <li key={gene.id}>
+            <a href={gene.id}>{gene.id}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default Home;
