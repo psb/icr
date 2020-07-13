b34ad1affdfd3ffad72d443e6cdedf01f7221792
Paul Bacchus <paul@paulbacchus.com>
2020-07-13 14:11:45 +0100

    Home page rendering all genes with selected information as cards. Had to manually edit DB data to make some of the features false as all were set to true.

--- app/client/src/Home.js
+++ app/client/src/Home.js
@@ -32,13 +32,45 @@ function Home() {
     return "Error loading data.";
   } else {
     return (
-      <ul>
+      <div className="row">
         {genes.map((gene) => (
-          <li key={gene.id}>
-            <a href={gene.id}>{gene.id}</a>
-          </li>
+          <div className="col-3 my-2">
+            <div className="card">
+              <img
+                src={gene.image}
+                className="card-img-top"
+                alt={gene.short_name}
+              />
+              <div className="card-body">
+                <h4 className="card-title">{gene.short_name}</h4>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  Is a drug target?
+                  {gene.is_druggable ? (
+                    <strong className="text-success"> Yes</strong>
+                  ) : (
+                    <strong className="text-danger"> No</strong>
+                  )}
+                </li>
+                <li className="list-group-item">
+                  Is an enzyme?
+                  {gene.is_enzyme ? (
+                    <strong className="text-success"> Yes</strong>
+                  ) : (
+                    <strong className="text-danger"> No</strong>
+                  )}
+                </li>
+              </ul>
+              <div className="card-body">
+                <a href={"/" + gene.id} className="btn btn-primary btn-block">
+                  More information
+                </a>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     );
   }
 }
